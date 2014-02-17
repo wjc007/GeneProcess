2e6c95621fdc53b03d68f7dc8dfec73fd913136e
Songli Li <jifucishi@vip.qq.com>
2014-02-17 15:58:30 +0800

    Initial Commit

---
+++ GeneProcess/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  GeneProcess
+//
+//  Created by Songli on 14-2-17.
+//  Copyright (c) 2014年 Songli. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
